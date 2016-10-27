4d4184b1c2a24e74f5ec30ce781d1b023ba7f40c
Kumar McMillan <kumar.mcmillan@gmail.com>
2016-10-27 16:28:53 +0100

    force push gh-pages
    This is really only necessary when changing the subtree split

--- slides/deploy.sh
+++ slides/deploy.sh
@@ -7,7 +7,7 @@ if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then
     echo "---------------------------------------------------------"
     exit 1
 fi
-git subtree split --prefix slides/www/ -b gh-pages && git push origin gh-pages:gh-pages && git branch -D gh-pages
+git subtree split --prefix slides/www/ -b gh-pages && git push -f origin gh-pages:gh-pages && git branch -D gh-pages
 if [[ $? -ne 0 ]]; then
     echo "hmm, there was an error"
     exit 1;
