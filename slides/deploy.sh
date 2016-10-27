8bdf4d4c89536c129c25cd29c59ffa30eee67a1d
Kumar McMillan <kumar.mcmillan@gmail.com>
2016-10-27 17:09:52 +0100

    handle deploy errors; build slides correctly

--- slides/deploy.sh
+++ slides/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-./build.sh
+set -e
+
+./slides/build.sh
 if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then
     git status
     echo "---------------------------------------------------------"
@@ -8,8 +10,4 @@ if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then
     exit 1
 fi
 git subtree split --prefix slides/www/ -b gh-pages && git push -f origin gh-pages:gh-pages && git branch -D gh-pages
-if [[ $? -ne 0 ]]; then
-    echo "hmm, there was an error"
-    exit 1;
-fi
 open "https://kumar303.github.io/web-ext-workshop/"
