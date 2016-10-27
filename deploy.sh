ae3828e4d5b48aa7a6d782b3e3e2d1c4e43bbd74
Kumar McMillan <kumar.mcmillan@gmail.com>
2016-10-27 11:48:06 +0100

    fix link

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then
     exit 1
 fi
 git subtree split --prefix www/ -b gh-pages && git push origin gh-pages:gh-pages && git branch -D gh-pages
-open "https://kumar303.github.io/web-ext-talk/"
+open "https://kumar303.github.io/web-ext-workshop/"
